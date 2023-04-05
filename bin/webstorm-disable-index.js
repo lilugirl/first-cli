0ee2dc3d1970f02f7eb9b828f091e607c29b68fb
yi7.liu <yi7.liu@symbio.com>
2023-04-05 22:31:45 +0800

    1.0.13

---
+++ bin/webstorm-disable-index.js
@@ -0,0 +1,2 @@
+#! /usr/bin/env node 
+console.log('webstorm-disable-index.js')
--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 'use strict'
-const path=require('path');
+console.log('install...')
+var os =require('os')
+function home(){
+    return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+}
 
+function homedir(){
+    var env=process.env;
+    var home =env.HOME;
+    var user = env.LOGNAME || env.USER || env.LNAME || env.USERNAME;
 
+    if(process.platform==='win32'){
+        return env.USERPROFILE || env.HOMEDRIVE + env.HOMEPATH || home || null
+    }
 
+    if(process.platform==='darwin'){
+        return home || (user? '/Users/'+user:null);
+    }
+
+    if(process.platform==='linux'){
+        return home || (process.getuid()===0?'/root':(user?'/home/'+user:null));
+    }
+
+    return home || null
+}
+
+module.exports = typeof os.homedir === 'function' ? os.homedir : homedir
+
+console.log(home())
+console.log(homedir())
+console.log(process.env.HOME)
